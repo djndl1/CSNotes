0f660ab203866870eda9df0ce4761da675527ea8
djndl1 <djndl1@gmail.com>
2019-08-20 23:59:58 +0800

    std::merge test

--- algorithm/introAlgorithm/c++-implmentation/chap.2/merge_sort.cc
+++ algorithm/introAlgorithm/c++-implmentation/chap.2/merge_sort.cc
@@ -6,7 +6,7 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-// a naive merge
+// a naive merge, C-ism
 void merge(vector<int>& A, size_t p, size_t q, size_t r)
 {
     size_t n1 = q - p + 1;
@@ -51,17 +51,46 @@ void merge_sort(vector<int>& A, size_t p, size_t r)
     }
 }
 
+#include <algorithm>
+#include <iterator>
+
+// bad implementation to accommodate C-like interface
+void merge_lazy(vector<int>& A, size_t p, size_t q, size_t r)
+{
+    auto zero = A.begin();
+    auto start = std::next(zero, p);
+    auto middle = std::next(zero, q+1);
+    auto end = std::next(zero, r+1);
+
+    vector<int> B(r - p + 1);
+    std::merge(start, middle, middle, end, B.begin());
+
+    for (auto sa = start, sb = B.begin(); sa != end && sb != B.end(); sa++, sb++) 
+        *sa = *sb;
+}
+
+void merge_sort_lazy(vector<int>& A, size_t p, size_t r)
+{
+    if (p < r) {
+        size_t q = (p + r) / 2;
+        merge_sort_lazy(A, p, q);
+        merge_sort_lazy(A, q+1, r);
+        merge_lazy(A, p, q, r);
+    }
+    
+}
+
 int main(int argc, char *argv[])
 {
     vector<int> A{31, 41, 59, 26, 42, 58};
 
-    vector<int> B{1, 3, 5, 2, 4, 6};
+    vector<int> B{1, 3, 5, 2, 4, 6, 8};
     merge(B,0, 2, B.size() - 1);
     for ( auto i : B )
         cout << i << ' ';
     cout << endl;
 
-    merge_sort(A, 0, A.size() - 1);
+    merge_sort_lazy(A, 0, A.size() - 1);
     for ( auto i : A )
         cout << i << ' ';
     cout << endl;
