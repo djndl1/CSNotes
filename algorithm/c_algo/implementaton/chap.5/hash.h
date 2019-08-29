09c8f1964d1ff4aa77942a1f9297abe3f558b3d5
djndl1 <djndl1@gmail.com>
2019-08-29 16:22:13 +0800

    hash functions

---
+++ algorithm/c_algo/implementaton/chap.5/hash.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef size_t index_t;
+
+#define hash_int(x, size) (x % size)
+
+index_t hash_str1(const char *key, size_t size);
+
+index_t hash_str2(const char *key, size_t size);
+
+index_t hash_str3(const char *key, size_t size);
