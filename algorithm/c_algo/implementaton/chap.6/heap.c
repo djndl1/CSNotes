162bebe6d7601912528abf0d19e890dd4aa1f0e8
djndl1 <djndl1@gmail.com>
2019-09-10 23:27:22 +0800

    heap sort finished

--- algorithm/c_algo/implementaton/chap.6/heap.c
+++ algorithm/c_algo/implementaton/chap.6/heap.c
@@ -2,21 +2,25 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-struct Element {
-        int dummy;
-};
 
 static inline int element_comp(const void *l, const void *r)
 {
-        element_t* lhs = l;
-        element_t* rhs = r;
+        const element_t* lhs = l;
+        const element_t* rhs = r;
         if (lhs->dummy > rhs->dummy)
                 return 1;
         else if (lhs->dummy < rhs->dummy)
                 return -1;
         else
                 return 0;
+}
+
+static inline void element_print(const void *self)
+{
+        const element_t* __self = self;
+        printf("%d", __self->dummy);
 }
 
 priority_queue_t priority_queue_ctor(size_t capacity)
@@ -45,10 +49,20 @@ bool priority_queue_is_full(priority_queue_t heap)
         return heap->size >= heap->capacity;
 }
 
+bool priority_queue_is_empty(priority_queue_t heap)
+{
+        return heap->size == 0;
+}
+
 int priority_queue_insert(priority_queue_t heap, element_t elm)
 {
         if (priority_queue_is_full(heap)) {
                 return 1;
+        }
+
+        if (priority_queue_is_empty(heap)) {
+                heap->elems[++heap->size] = elm;
+                return 0;
         }
 
         size_t i;
@@ -61,11 +75,6 @@ int priority_queue_insert(priority_queue_t heap, element_t elm)
         return 0;
 }
 
-bool priority_queue_is_empty(priority_queue_t heap)
-{
-        return heap->size == 0;
-}
-
 element_t priority_queue_delete_min(priority_queue_t heap)
 {
         if (priority_queue_is_empty(heap))
@@ -74,9 +83,9 @@ element_t priority_queue_delete_min(priority_queue_t heap)
         element_t last = heap->elems[heap->size--];
 
         size_t i, child;
-        for (i = 1; i * 2 <= heap->size; i = child) {
+        for (i = 1; i*2 <= heap->size; i = child) {
                 child =  i * 2;
-                if ( child != heap->size &&
+                if ( child < heap->size &&
                      element_comp(&heap->elems[child+1], &heap->elems[child]) < 0)
                         child++;
 
@@ -86,17 +95,17 @@ element_t priority_queue_delete_min(priority_queue_t heap)
                         break;
         }
         heap->elems[i] = last;
+
         return min;
-        
 }
 
-void priority_queue_make_heap_order(priority_queue_t heap)
+void priority_queue_adjust_root(priority_queue_t heap, size_t cur_root)
 {
         if (priority_queue_is_empty(heap))
                 return;
-        element_t root = heap->elems[1];
+        element_t root = heap->elems[cur_root];
         size_t i, child;
-        for (i = 1; i * 2 <= heap->size; i = child) {
+        for (i = cur_root; i * 2 <= heap->size; i = child) {
                 child =  i * 2;
                 if ( child != heap->size &&
                      element_comp(&heap->elems[child+1], &heap->elems[child]) < 0)
@@ -108,4 +117,29 @@ void priority_queue_make_heap_order(priority_queue_t heap)
                         break;
         }
         heap->elems[i] = root;
+}
+
+void priority_queue_make_heap_order(priority_queue_t heap)
+{
+        for (size_t i = heap->size / 2; i > 0; i--)
+                priority_queue_adjust_root(heap, i);
+}
+
+void priority_queue_sort_heap(priority_queue_t heap)
+{
+        size_t old_size = heap->size;
+        for (size_t i = 0; i < old_size; i++) {
+                element_t min = priority_queue_delete_min(heap);
+                //priority_queue_print(heap);
+                heap->elems[heap->size+1] = min;
+        }
+        heap->size = old_size;
+}
+
+void priority_queue_print(priority_queue_t heap)
+{
+        for (size_t i = 1; i <= heap->size; i++) {
+                element_print(&heap->elems[i]);
+                putchar(' ');
+        }
 }
---
+++ algorithm/c_algo/implementaton/chap.6/heap_sort_test.c
@@ -0,0 +1,27 @@
+#include "heap.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+        priority_queue_t h;
+
+        
+        h = priority_queue_ctor(10);
+        srand(time(0));
+        for (size_t i = 1; i <= 10; i++) {
+                element_t tmp = { (rand()) / 100000000 + 1 };
+                priority_queue_insert(h, tmp);
+        }
+//        priority_queue_make_heap_order(h);
+        priority_queue_print(h);
+        putchar('\n');
+        priority_queue_sort_heap(h);
+        priority_queue_print(h);
+        putchar('\n');
+
+        return 0;
+}
+
