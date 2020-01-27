029491193d2e29035724393ba6359180c3babe3b
djndl1 <djndl1@gmail.com>
2020-01-27 20:24:59 +0800

    naive peak finder fixed

--- algorithm/introAlgorithm/python-implementation/peak_finder_1d.py
+++ algorithm/introAlgorithm/python-implementation/peak_finder_1d.py
@@ -6,11 +6,11 @@ def naive_peak_finder_1d(list):
         return None
     for i in range(1, len(list) - 1):
         if list[i] >= list[i-1] and list[i] >= list[i+1]:
-            return list[i]
+            return i
     if list[0] >= list[1]:
-        return list[0]
+        return 0
     if list[-1] >= list[-2]:
-        return list[-1]
+        return len(list) - 1
     return None
 
 def __recursive_peak_finder_1d(list, start_index):
