3d2ccd401afaa1143872ead6a13f9089bb2d6633
djndl1 <djndl1@gmail.com>
2020-03-08 01:26:53 +0800

    A STL-like C vector

--- algorithm/c_algo/implementaton/chap.3/vector_test.c
+++ algorithm/c_algo/implementaton/chap.3/vector_test.c
@@ -1,31 +1,51 @@
 #include "vector.h"
+#include "../lib/utils.h"
+#include "../lib/sort.h"
 
 #include <stdio.h>
 
-void inc(int *pi) { (*pi)++; }
+vector_type_register(int);
+
+void inc(int* pi)
+{
+        (*pi)++;
+}
 
 int main(int argc, char *argv[])
 {
-        vec_t(int, 30, NULL, NULL, a);
+        vector_init(int, 30, NULL, vec);
 
-        for_each_in(i, a)
-                AT(a, i) = 5;
+        vector_for_each(i, vec) {
+                vector_at(vec, i) = 60 - 2 * i;
+                inc(&vector_at(vec, i));
+        }
 
-        for_each_do(a, inc);
-
-        for_range(0, SIZE(a), i)
-                printf("%d ", AT(a, i));
-        putchar('\n');
-        PUSH_BACK(10, a);
-        PUSH_BACK(20, a);
-        PUSH_BACK(30, a);
-        PUSH_BACK(40, a);
-        vector_ops(int, push_back)(&a, 50);
-
-        for_range(0, SIZE(a), i) printf("%d ", AT(a, i));
+        vector_for_range(0, 30, _)
+                printf("%d ", vector_at(vec, _));
         putchar('\n');
 
-        DELETE(a);
+        vector_push_back(int, vec, 10);
+        vector_push_back(int, vec, 20);
+        vector_push_back(int, vec, 30);
+        vector_push_back(int, vec, 40);
+
+        vector_init(int, 20, NULL, arr);
+        vector_for_each(i, arr)
+                vector_at(arr, i) = i;
+
+        INSERTION_SORT(int, vector_begin(vec), vector_end(vec));
+
+        vector_pop_back(int, vec);
+        vector_insert(int, vec, 20, 100);
+        vector_swap(int, vec, arr);
+        vector_for_each_in(int, elem, vec) {
+                printf("%d ", *elem);
+        }
+        putchar('\n');
+
+        vector_for_each_in(int, elem, arr) { printf("%d ", *elem); }
+        putchar('\n');
+
+        vector_delete(int, vec);
         return 0;
 }
-
