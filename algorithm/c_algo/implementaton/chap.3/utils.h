df2fc3b8d1c158afe2c3e026aac4892f49528865
djndl1 <djndl1@gmail.com>
2020-03-09 22:01:08 +0800

    add some default comparators

--- algorithm/c_algo/implementaton/chap.3/utils.h
+++ algorithm/c_algo/implementaton/chap.3/utils.h
@@ -1,4 +1,25 @@
 #pragma once
 
+#include <stdbool.h>
+
 #define basic_swap(type, a, b)                         \
     { type tmp = a; a = b; b = tmp; }
+
+#define type_predicate_register(type)           \
+    typedef bool (*type##_binary_predicate)(type, type);    \
+    typedef int (*type##_spaceship_comparator(type, type));    \
+                                                               \
+    bool type##_default_binary_predicate(type a, type b) \
+    {                                                    \
+        return a == b;                                   \
+    }                                                    \
+                                                         \
+    int type##_default_spaceship_comparator(type a, type b) \
+    {                                                       \
+        if (a > b)                                          \
+            return 1;                                       \
+        else if (a < b) return -1;                          \
+        else return 0;                                      \
+    }
+
+
