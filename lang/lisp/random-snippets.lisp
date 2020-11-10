34dd09677a30206f06fbfe4cf3145d52d705db00
djndl1 <djndl1@gmail.com>
2020-11-10 22:27:30 +0800

    list

--- lang/lisp/random-snippets.lisp
+++ lang/lisp/random-snippets.lisp
@@ -19,3 +19,50 @@
           0
           (let ((z (my-index x (cdr y))))
                 (and z (+ z 1))))))
+(defun new-union-r (rx ry)
+  (if (null ry)
+      rx
+      (new-union-r (adjoin (car ry) rx) (cdr ry))))
+
+(defun new-union (x y)
+  (reverse (new-union-r (reverse x) y)))
+
+(defun our-assoc (key alist)
+  (and (consp alist)
+       (let ((pair (car alist)))
+         (if (eql key (car pair))
+             pair
+             (our-assoc key (cdr alist))))))
+
+(defun occurrences-impl (lst dict)
+  (if (null lst)
+      dict
+      (let* ((item (car lst))
+            (cnt (count item lst)))
+        (occurrences-impl (remove item lst)
+                          (cons (cons item cnt) dict)))))
+(defun occurrences (lst)
+  (sort (occurrences-impl lst nil) #'> :key #'cdr))
+
+(defun pos+ (lst)
+  (pos+-impl nil lst 0))
+(defun pos+-impl (prev remaining pos)
+  (if (null remaining)
+      prev
+      (pos+-impl  (append prev (list (+ (car remaining) pos)))
+                  (cdr remaining)
+                  (+ 1 pos))))
+
+(defun construct-end (len)
+  (concatenate 'string "NIL" (make-string len :initial-element (char ")" 0))))
+
+(defun showdots-impl (head tail-lst pos)
+  (if (null tail-lst)
+      (concatenate 'string head (construct-end pos))
+      (showdots-impl (concatenate 'string
+                                  head "(" (write-to-string (car tail-lst)) " . ")
+                     (cdr tail-lst)
+                     (+ 1 pos))))
+
+(defun showdots (lst)
+  (showdots-impl "" lst 0))
