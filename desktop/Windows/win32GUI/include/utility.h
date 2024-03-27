08cd420c558f8ee74205e6a779e9e46f1209184c
djndl1 <djndl1@gmail.com>
2024-03-27 14:29:30 +0000

    remove codes

--- desktop/Windows/win32GUI/include/utility.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void CenterWindow(HWND win);
--- desktop/Windows/win32GUI/src/MessageBox.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
-{
-        MessageBoxW(NULL, L"First Program", L"First", MB_OK);
-
-        return 0;
-}
